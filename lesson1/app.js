11e16f431fa6ead2cacbf10d0f509794468b23ba
sarkermajid <majidursarkar333@gmail.com>
2022-07-16 18:18:00 +0600

    html file,json data,header & cookies

--- lesson1/app.js
+++ lesson1/app.js
@@ -4,9 +4,27 @@ const userRouter = require('./routes/users.route');
 
 app.use('/api/user/',userRouter);
 
-app.get('/',(req,res)=>{
-    res.send('I am a get method at home route');
+app.use('/register',(req,res)=>{
+    res.statusCode = 200;
+    res.sendFile(__dirname+'/views/register.html');
+    // res.redirect('/login');
+});
+
+app.use('/login',(req,res)=>{
+    res.cookie('name','sarker majid');
+    res.cookie('age','25');
+    res.append('id','183039');
+    res.json({
+        name : 'sarker majid',
+        profession : 'Software engineer',
+        statusCode : 200,
+    })
+    // res.clearCookie('name');
     res.end();
+})
+app.use('/',(req,res)=>{
+    res.statusCode = 200;
+    res.sendFile(__dirname+'/views/index.html');
 });
 app.use((req,res)=>{
     res.send('404 ! Not a valid url');
