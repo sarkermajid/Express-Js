4b2d80b92137de16f18e9b83a2e7fa832a7fcf36
sarkermajid <majidursarkar333@gmail.com>
2022-07-16 23:20:11 +0600

    route parameter

--- lesson2/index.js
+++ lesson2/index.js
@@ -3,11 +3,22 @@ const app = express();
 const hostname = '127.0.0.1';
 const PORT = 3000;
 
-app.get('/',(req,res)=>{
-    const name = req.query.name;
-    const id = req.query.id;
-    res.send(`<h1>Student Name is : ${name} and Id is : ${id}</h1>`);
-    res.end();
+// query parameter
+// app.get('/',(req,res)=>{
+//     const name = req.query.name;
+//     const id = req.query.id;
+//     res.send(`<h1>Student Name is : ${name} and Id is : ${id}</h1>`);
+//     res.end();
+// })
+
+
+// route parameter
+app.get('/studentName/:name/studentId/:id/studentAge/:age',(req,res)=>{
+   const name = req.params.name;
+   const id = req.params.id;
+   const age = req.params.age;
+   res.send(`<h1>Student Name is : ${name} , Id is : ${id} & Age is : ${age}</h1>`);
+   res.end();
 })
 
 app.listen(PORT,()=>{
