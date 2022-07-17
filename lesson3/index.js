90ba6b3f7d5695bb3b70c54b1cfbae5c60bc7e34
sarkermajid <majidursarkar333@gmail.com>
2022-07-17 14:58:37 +0600

    setup environment variable

---
+++ lesson3/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req,res)=>{
+    res.send('Hello');
+})
+app.listen(PORT,()=>{
+    console.log(`server is running at http://localhost:${PORT}`);
+})
